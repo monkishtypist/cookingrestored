474d5a628c4d2f21ea15e2ed875eda87bb8c3384
Tim Spinks <tim@monkishtypist.com>
2019-12-17 16:24:56 -0800

    theme details updates, adsense js to head

--- functions.php
+++ functions.php
@@ -10,3 +10,10 @@ add_action( 'wp_enqueue_scripts', 'penci_soledad_child_scripts' );
 /*
  * All custom functions go here
  */
+
+function adsense_scripts() {
+    $ad_client = 'ca-pub-4769896344362494';
+    $ad_src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+    echo sprintf( '<script data-ad-client="%1$s" async src="%2$s"></script>', $ad_client, $ad_src );
+}
+add_action( 'wp_head', 'adsense_scripts' );
