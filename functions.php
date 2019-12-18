e974ad83f69a03310aad6ab1347ce050b4f9086a
Tim Spinks <tim@monkishtypist.com>
2019-12-17 17:02:02 -0800

    adsense js to head

--- functions.php
+++ functions.php
@@ -12,6 +12,10 @@ add_action( 'wp_enqueue_scripts', 'penci_soledad_child_scripts' );
  */
 
 function adsense_scripts() {
+    // $ad_client = 'ca-pub-1346987114864425'; // OLD
+    // echo sprintf( '<script async src="%1$s"></script>', $ad_src ); // OLD
+    // echo sprintf( '<script>(adsbygoogle = window.adsbygoogle || []).push({ google_ad_client: "%1$s", enable_page_level_ads: true });</script>', $ad_client ); // OLD
+
     $ad_client = 'ca-pub-4769896344362494';
     $ad_src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
     echo sprintf( '<script data-ad-client="%1$s" async src="%2$s"></script>', $ad_client, $ad_src );
